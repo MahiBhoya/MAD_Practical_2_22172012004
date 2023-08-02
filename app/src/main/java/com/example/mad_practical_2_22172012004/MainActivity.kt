0fa9a45dcf47f9a9be188d3b3b05df02981d9e62
student <student@gnu.ac.in>
2023-08-02 08:34:33 +0530

    Practical_2

--- app/src/main/java/com/example/mad_practical_2_22172012004/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_2_22172012004/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.mad_practical_2_22172012004
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     val  TAG = "MainActivity"
@@ -16,11 +19,29 @@ class MainActivity : AppCompatActivity() {
     fun showMessage(message:String){
         Log.i(TAG, message)
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
+        val obj:ConstraintLayout?=findViewById(R.id.mainConstrain)
+        if(obj != null)
+            Snackbar.make(obj,message,Snackbar.LENGTH_SHORT).show()
     }
 
     override fun onStart() {
         super.onStart()
         showMessage("onStart message is called")
     }
+
+    override fun onResume() {
+        super.onResume()
+        showMessage("onResume message is called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        showMessage("onPause message in called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showMessage("onStop message in called")
+    }
+
 }
